c850ae044607794a45994486f113e1b90ce42dcd
habuild <36288425+habuild@users.noreply.github.com>
2022-03-07 00:36:05 +1100

    Update sensor.py

--- custom_components/powerpal-test/sensor.py
+++ custom_components/powerpal-test/sensor.py
@@ -214,5 +214,8 @@ class PowerpalLastTimestampSensor(PowerpalSensor, SensorEntity):
         """Return the icon of the sensor."""
         return "mdi:clock-outline"
 
-        
+    @property
+    def device_class(self) -> str:
+        """Return the device class, if any."""
+        return SensorDeviceClass.TIMESTAMP    
     
