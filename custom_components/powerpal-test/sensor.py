c4559922e964189583c81986695d98853da643d0
habuild <36288425+habuild@users.noreply.github.com>
2022-03-09 00:59:23 +1100

    Update sensor.py

--- custom_components/powerpal-test/sensor.py
+++ custom_components/powerpal-test/sensor.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import date, datetime, timedelta, timezone
+from datetime import date, datetime, timedelta, timezone, timestamp
 from typing import Any, Final, cast, final
 
 from homeassistant.components.sensor import (
